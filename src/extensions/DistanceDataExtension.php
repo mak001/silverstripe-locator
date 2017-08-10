ed4a98bc4c17d75f0dcba61bf7fce11568753f58
mak001 <mak001001@gmail.com>
2017-08-10 16:53:52 -0500

    Got distances to work (possibly)   - not fully hooked up with react

--- src/extensions/DistanceDataExtension.php
+++ src/extensions/DistanceDataExtension.php
@@ -3,6 +3,7 @@
 namespace Dynamic\Locator;
 
 use Dynamic\SilverStripeGeocoder\GoogleGeocoder;
+use SilverStripe\Control\HTTPRequest;
 use SilverStripe\ORM\DataExtension;
 use SilverStripe\ORM\Queries\SQLSelect;
 use SilverStripe\ORM\DataQuery;
@@ -16,7 +17,9 @@ class DistanceDataExtension extends DataExtension
      */
     public function augmentSQL(SQLSelect $query, DataQuery $dataQuery = null)
     {
-        $address = Controller::curr()->getRequest()->getVar('Address');
+        $variables = $this->getRequestVariables(Controller::curr()->getRequest());
+        $address = $variables['address'];
+
         if ($this->owner->hasMethod('updateAddressValue')) {
             $address = $this->owner->updateAddressValue($address);
         }
@@ -33,5 +36,25 @@ class DistanceDataExtension extends DataExtension
                     ));
             }
         }
+    }
+
+    /**
+     * From https://github.com/silverstripe/silverstripe-graphql/blob/e39e59483310923cbf14f5f4baa44033c14a270c/src/Controller.php#L60-L70
+     *
+     * @param HTTPRequest $request
+     * @return array|mixed|null
+     */
+    public function getRequestVariables(HTTPRequest $request)
+    {
+        $contentType = $request->getHeader('Content-Type') ?: $request->getHeader('content-type');
+        $isJson = preg_match('#^application/json\b#', $contentType);
+        if ($isJson) {
+            $rawBody = $request->getBody();
+            $data = json_decode($rawBody ?: '', true);
+            $variables = isset($data['variables']) ? (array)$data['variables'] : null;
+        } else {
+            $variables = json_decode($request->requestVar('variables'), true);
+        }
+        return $variables;
     }
 }
